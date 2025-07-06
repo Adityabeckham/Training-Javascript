7bbeb8bfc4fa5519faa4adffa70c6d8615bdbedd
Aditya Beckham <abfirmansyah01@gmail.com>
2025-07-06 17:16:00 +0700

    balikKata.js

--- latihan js/balikKata.js
+++ latihan js/balikKata.js
@@ -1,6 +1,12 @@
 // BALIK KATA
 function balikKata(str) {
+    // Validasi input, pastikan str adalah string
+    if (typeof str !== 'string') {
+        return 'Input harus berupa string!';
+    }
     return str.split('').reverse().join('');
 }
 
-console.log(balikKata("JAVASCRIPT")); // OUTPUT TPIRCSAVAJ
+console.log(balikKata("JAVASCRIPT"));  // Output: TPIRCSAVAJ
+console.log(balikKata(12345));         // Output: Input harus berupa string!
+console.log(balikKata(true));          // Output: Input harus berupa string!
